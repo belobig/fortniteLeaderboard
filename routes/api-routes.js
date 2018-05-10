10e413c606fd9be0063c125f7f89676955581d09
Jason J. Anderson <jasonjanderson@outlook.com>
2018-05-10 17:42:29 -0600

    bug fixes to get page loading

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,32 +3,30 @@ var express = require("express");
 var router = express.Router();
 
 // Routes
-module.exports = function () {
-	//GET route
-	router.get("/", function (req, res) {
-		db.User.findAll().then(function (results) {
-			console.log("Got here!");
-			var userObj = {
-				users: results
-			};
-			res.render("index", userObj);
-			// res.json(results);
-			// console.log(results);
-		});
+//GET route
+router.get("/", function (req, res) {
+	db.User.findAll().then(function (results) {
+		console.log("Got here!");
+		var userObj = {
+			users: results
+		};
+		res.render("index", userObj);
+		// res.json(results);
+		// console.log(results);
 	});
+});
 
-	// POST route
-	router.post("/", function (res, req) {
-		var user = req.body;
-		db.User.create({
-			nickname: user.nickname,
-			awesomeness_level: user.awesomeness_level,
-			age: user.age,
-			class: user.class
-		}).then(function(dbUser) {
-			res.json(dbUser);
-			console.log(dbUser);
-		});
+// POST route
+router.post("/", function (res, req) {
+	var user = req.body;
+	db.User.create({
+		nickname: user.nickname,
+		awesomeness_level: user.awesomeness_level,
+		age: user.age,
+		class: user.class
+	}).then(function (dbUser) {
+		res.json(dbUser);
+		console.log(dbUser);
 	});
-	return router;
-}
+});
+module.exports = router;

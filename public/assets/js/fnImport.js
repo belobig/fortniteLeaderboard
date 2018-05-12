b4a7d843c4724bdcce9c11f769c146981b6e54de
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-05-12 10:38:23 -0600

    Getting API responses!!

--- public/assets/js/fnImport.js
+++ public/assets/js/fnImport.js
@@ -1,15 +1,45 @@
-$(document).ready(function () {
-var key = "92bf5651-fef6-494e-a03d-52081c2382e4"
-	function getStats() {
-		$.ajax({
-			method: "GET",
-			auth: key,
-			url: "https://api.fortnitetracker.com/v1/profile/pc/belobig"
-		}).then(function (response) {
-			console.log(response);
-		});
-	}
+// $(document).ready(function () {
 
-	getStats();
+var FortniteAPI = require("fortnite-api-node");
 
-});
+var options = {
+	key: "92bf5651-fef6-494e-a03d-52081c2382e4",
+	platform: "pc"
+}
+
+var fortnite = new FortniteAPI(options);
+
+
+
+// fortnite.getData(example_player)
+// .then(function(result){
+//     console.log(result);
+// });
+
+// var players = ["belobig", "waffles", "praxiSiphion"];
+
+// fortnite.getPlayers(players)
+// .then(function(result){
+//     console.log(result);
+// });
+
+// module.exports = fortnite;
+
+// var key = "92bf5651-fef6-494e-a03d-52081c2382e4";
+
+// 	function getStats() {
+// 		$.ajax({
+// 			method: "GET",
+// 			headers:  {
+// 				'TRN-Api-Key': key,
+// 				'Access-Control-Allow-Origin': '*'
+// 			},
+// 			url: "https://api.fortnitetracker.com/v1/profile/pc/belobig"
+// 		}).then(function (response) {
+// 			console.log(response);
+// 		});
+// 	}
+
+// 	getStats();
+
+// });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,11 @@
 var db = require("../models");
+// var fortnite = require("../public/assets/js/fnImport");
 var express = require("express");
+var axios = require("axios");
 var router = express.Router();
+
+var example_player = "belobig";
+var key = "92bf5651-fef6-494e-a03d-52081c2382e4";
 
 // Routes
 //GET route
@@ -11,8 +16,24 @@ router.get("/", function (req, res) {
 			users: results
 		};
 		res.render("index", userObj);
+
 		// res.json(results);
 		// console.log(results);
+	});
+});
+
+// Another GET route for data from Fortnite Tracker API
+router.get("/leaderboard", function (req, res) {
+	console.log("Leaderboard route");
+	axios({
+		method: "get",
+		url: "https://api.fortnitetracker.com/v1/profile/pc/belobig",
+		headers: {
+			'TRN-Api-Key': key
+		}
+	}).then(function(response) {
+		res.render("leaderboard", {response: response});
+		console.log("Response: ", response.data.stats);
 	});
 });
 
